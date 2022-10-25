ea9979f4fcad4039cbec364c93ca99b400b5ea2d
Aarush06 <89701094+Aarush06@users.noreply.github.com>
2022-10-25 13:27:14 -0600

    p1

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// Triangle Perimeter Assignment Start Code
+let firstName = document.getElementById("FirstName");
+let lastName = document.getElementById("LastName");
+let allNick = document.getElementById("AllNick");
+let randNick = document.getElementById("RandNick");
+let container = document.getElementById("container");
+
+let d = firstName.value;
+
+let array = [
+  "Crusher",
+  "the Scientist",
+  "Twinkle-toes",
+  "the Coder",
+  "the Jester",
+  "the Sloth",
+  "Quick-Silver",
+];
+
+document.getElementById("RandNick").addEventListener("click", clickHandler);
+
+// function Randomint(low,high){
+// return Math.floor(Math.random() * (high))
+// }
+
+function clickHandler() {
+  let outputStr = "";
+  outputStr += ` ${d}   ${array[Math.floor(Math.random() * 7)]} ${lastName}`;
+  container.innerHTML = outputStr;
+}
