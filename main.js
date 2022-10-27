497ee5e87357fc974a2c18673642532b7f5f737d
Aarush06 <89701094+Aarush06@users.noreply.github.com>
2022-10-27 11:12:37 -0600

    d

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-let firstName = document.getElementById("FirstName");
-let lastName = document.getElementById("LastName");
-let allNick = document.getElementById("AllNick");
-let randNick = document.getElementById("RandNick");
+let firstName = document.getElementById("FirstNameInput");
+let lastName = document.getElementById("LastNameInput");
+let allNick = document.getElementById("AllNickButton");
+let randNick = document.getElementById("RandNickButton");
 let container = document.getElementById("container");
-
-let d = firstName.value;
 
 let array = [
   "Crusher",
@@ -13,17 +11,26 @@ let array = [
   "the Coder",
   "the Jester",
   "the Sloth",
-  "Quick-Silver",
 ];
 
-document.getElementById("RandNick").addEventListener("click", clickHandler);
+document
+  .getElementById("RandNickButton")
+  .addEventListener("click", randNickname);
 
-// function Randomint(low,high){
-// return Math.floor(Math.random() * (high))
-// }
-
-function clickHandler() {
+function randNickname() {
   let outputStr = "";
-  outputStr += ` ${d}   ${array[Math.floor(Math.random() * 7)]} ${lastName}`;
+  outputStr += ` ${firstName.value} ${
+    array[Math.floor(Math.random() * array.length)]
+  } ${lastName.value}`;
+  container.innerHTML = outputStr;
+}
+
+document.getElementById("AllNickButton").addEventListener("click", AllNickname);
+
+function AllNickname() {
+  let outputStr = "";
+  for (let i = 0; i < array.length; i++) {
+    outputStr += `${firstName.value} ${array[i]} ${lastName.value}<hr>`;
+  }
   container.innerHTML = outputStr;
 }
